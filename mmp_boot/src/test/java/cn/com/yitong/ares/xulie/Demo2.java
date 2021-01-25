e359a4d1b1bf98d662914ee8258efed022827988
532960400@qq.com <xy404044>
2021-01-25 09:54:07 +0800

    代码提交

--- mmp_boot/src/test/java/cn/com/yitong/ares/xulie/Demo2.java
+++ mmp_boot/src/test/java/cn/com/yitong/ares/xulie/Demo2.java
@@ -4,6 +4,7 @@ import java.util.ResourceBundle;
 
 /** 读取资源文件ResourseBundle */
 public class Demo2 {
+
     public static void main(String[] args) {
         // 读取资源文件的名称，但是没有后缀，资源文件保存在CLASSPATH中
         ResourceBundle resourceBundle = ResourceBundle.getBundle("co.com.yitong.ReadMe");
